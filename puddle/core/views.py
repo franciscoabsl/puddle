85c90f8fb7756e3cd52a9ae7da8dabd18e8528ff
franciscoabsl <franciscosouzalima@gmail.com>
2024-06-08 17:57:08 -0300

    feat: add login logic

--- puddle/core/views.py
+++ puddle/core/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from item.models import Category, Item
 
-from .forms import SignupForm
+from .forms import SignupForm, LoginForm
 
 
 def index(request):
